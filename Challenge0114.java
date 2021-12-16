20471629eec893176b36f2f6b237c18f77ef2190
Klavier <klaviernavarro@gmail.com>
2021-12-16 18:17:00 +0100

    Challenge 0114 - Último dígito del factorial

---
+++ Challenge0114.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class Challenge0114 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int num = 0;
+		int casos = sc.nextInt();
+		for(int i = 0;i < casos;i++) {
+			num = sc.nextInt();
+			if(num == 0 || num == 1){
+				System.out.println(1);
+				}
+				else if(num == 2) {
+					System.out.println(2);
+				}
+				else if(num == 3) {
+					System.out.println(6);
+				}
+				else if(num == 4) {
+					System.out.println(4);
+				}
+				else {
+					System.out.println(0);
+				}
+			}
+		}
+	}
