b0c47e15ac876222358d43ced2d88d06666c978a
Klavier <klaviernavarro@gmail.com>
2021-12-16 18:17:14 +0100

    Challenge 0116 - ¡Hola mundo!

---
+++ Challenge0116.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class Challenge0116{
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		byte num = sc.nextByte();
+		for(byte i = 0;i < num;i++){
+			System.out.println("Hola mundo.");
+		}
+	}
+}
